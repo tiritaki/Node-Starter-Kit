b3eb59354915c4e138951b33be0f7b39bf5de26f
Oleh <Oleh@progressive-id.co.uk>
2023-03-23 16:39:53 +0000

    feat: playing with end points

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express()
+
+app.get("/love", (req, res) => {res.send("This is a love response")})
+app.get("/node", (req, res) => {res.send("This is a node response")})
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000. Ready to accept requests!')
+})

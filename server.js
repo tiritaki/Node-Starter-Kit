dbd1c5839599fbb612767e02d162c94fb34a8dfc
Oleh <Oleh@progressive-id.co.uk>
2023-03-31 11:09:53 +0100

    feat: adding req.params

--- server.js
+++ server.js
@@ -22,22 +22,25 @@ const albumsData = [
       primaryGenreName: "Pop",
       url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
     },
+    {
+        albumId: "12",
+        artistName: "BeyoncéNew",
+        collectionName: "Dangerously In Love",
+        artworkUrl100:
+          "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
+        releaseDate: "2003-06-24T07:00:00Z",
+        primaryGenreName: "Pop",
+        url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
+      },
   ];
 
 app.get("/albums", (req, res) => {res.send(albumsData)})
 
 app.get("/albums/:albumId", (req, res) => {res.send(
-    albumsData.filter(item => item.albumId.includes(req.query.id)))})
-
-    
-app.get("/albums/:albumId", (req, res) => {res.send(
         albumsData.filter(item => item.albumId.includes(req.params.albumId)))})
-    
-    
 
-
-
-
+// app.get("/albums/:albumId", (req, res) => {res.send(req.params.albumId)})
+       
 app.listen(3000, () => {
     console.log('Server is listening on port 3000. Ready to accept requests!')
 })
